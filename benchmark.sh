7347b3f3863ad74a54909e30d168564e4b544fb2
Gjorgji Jankovski <j.gjorgji@gmail.com>
2016-02-22 23:45:28 +0100

    fix logic and syntax errors

--- benchmark.sh
+++ benchmark.sh
@@ -8,15 +8,15 @@ SCALE_FACTOR=1
 FORCE=false
 QUERY_MODE="sequential"
 CACHE_MODE="both"
-HOST=""
+HOST=
 USER="postgres"
 
-if [ -z ${DBGEN_LOCATION} ]; then
+if [[ -z ${DBGEN_LOCATION} ]]; then
     echo "Need to specify DBGEN_LOCATION env variable"
     exit 1
 fi
 
-while getopts "s:fq:c:h:" opt; do
+while getopts "s:q:c:h:f" opt; do
     case ${opt} in
         s)
             SCALE_FACTOR=${OPTARG}
@@ -44,24 +44,29 @@ PSQL="$(which psql) -U ${USER} -h ${HOST}"
 
 generate_data () {
     pushd ${DBGEN_LOCATION}
-    DBGEN_ARGS="-s ${SCALE_FACTOR} -T a"
-    if [ ${FORCE} ]; then
-        DBGEN_ARGS="${DBGEN_ARGS} -f"
+    # Only generate the 
+    if [[ ${FORCE} == true ]]; then
+        rm *.tbl
+        ./dbgen -s "${SCALE_FACTOR}" -T a -f
     fi
-    ./dbgen ${DBGEN_ARGS}
 
     # Now we need to remove the last delimiter so postgres can import the data
     for file in *.tbl; do
-        sed -i 's/|$//' ${file} &
+        # Check if the file has been processed once
+        firstline=$(head -n 1 "${file}")
+        # Intentional whitespace in the below line
+        if [[ "${firstline: -1}" != '|' ]]; then
+            echo "EXECUTING CONTINUE"
+            continue
+        fi
+        sed -i 's/|$//' ${file}
     done
-    wait
     popd
     # Data should now be nicely formatted for import
 }
 
 create_schema_and_tables () {
-    #${PSQL} -c "DROP SCHEMA ssb IF EXISTS CASCADE;"
-    ${PSQL} -c "CREATE SCHEMA IF NOT EXISTS ssb;"
+    ${PSQL} -c "CREATE SCHEMA ssb;"
     ${PSQL} < ${SCHEMA_FILE}
 }
 
@@ -93,11 +98,10 @@ main () {
     generate_data
     
     # Force needs to always be specified when changing scale factor between runs!!!
-    if [ ${FORCE} ]; then
+    if [[ ${FORCE} == true ]]; then
         ${PSQL} -c "DROP SCHEMA IF EXISTS ssb CASCADE;"
+        create_schema_and_tables
     fi
-
-    create_schema_and_tables
 
     load_data
 
