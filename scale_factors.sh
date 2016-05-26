0a4a307fea712e319176af917ef431c151916424
Gjorgji Jankovski <j.gjorgji@gmail.com>
2016-05-26 11:43:21 +0200

    fix runall script

--- scale_factors.sh
+++ scale_factors.sh
@@ -1,4 +1,6 @@
-factors=(1 10 30 100);
+#! /usr/bin/env bash
+
+sfactors=(1 10 30 100);
 for sfactor in "${sfactors[@]}"; do
-        DBGEN_LOCATION=/root/ssb-dbgen SCHEMA_FILE=schema/dss-ssb.ddl RESULTDIR=/root/results ./benchmark.sh -s "${sfactor}" -f -h pgpool1
+        DBGEN_LOCATION=/root/ssb-dbgen SCHEMA_FILE=schema/dss-ssb.ddl RESULTDIR=/root/results ./benchmark.sh -s "${sfactor}" -f -h pgpool1 -p 5433
 done;
